bca7270000a0474496c49cd4bea2893f14bfa16e
PaulinaWalconce <alma.lopez3372@alumnos.udg.mx>
2026-03-15 09:38:00 -0600

    Add class to product image in ProductCard
    se ajusta tamaño de las ímagenes 2048x2048

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,7 +3,11 @@ import { Link } from "react-router-dom";
 function ProductCard({ product }) {
   return (
     <div className="card">
-      <img src={product.image} alt={product.name} />
+      <img 
+        src={product.image} 
+        alt={product.name} 
+        className="product-image"
+      />
       <h3>{product.name}</h3>
       <p>${product.price}</p>
       <Link to={`/product/${product.id}`}>
